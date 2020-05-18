e5737240f9834be7bdc0cfa8187aca0445d3f0f0
Ivo Branco <ivo.branco@fccn.pt>
2020-05-18 15:42:43 +0100

    Print PDF inline or as an attachment. Print PDF Landscape or Portrait.

--- app.py
+++ app.py
@@ -6,17 +6,13 @@ from nau.course.certificate.configuration import Configuration
 
 app = Flask(__name__)
 
-#@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def redirect(path):
-
+def convert_certificate_to_pdf(content_disposition, path):
     configuration = Configuration('config.yml')
     config = configuration.config()
 
     lms_server_url = config['LMS_SERVER_URL'] # https://lms.dev.nau.fccn.pt
     certificate_file_name = config['CERTIFICATE_FILE_NAME'] # certificate.pdf
     # change inline to attachment if you want the file to download rather than display in the browser
-    content_disposition = 'inline'
     url = lms_server_url + '/' + path
 
     binary_pdf = CourseCertificateToPDF(config).convert(url)
@@ -25,6 +21,14 @@ def redirect(path):
     response.headers['Content-Type'] = 'application/pdf'
     response.headers['Content-Disposition'] = '{}; filename={}'.format(content_disposition, certificate_file_name)
     return response
+
+@app.route('/inline/<path:path>')
+def inline(path):
+    return convert_certificate_to_pdf("inline", path)
+
+@app.route('/attachment/<path:path>')
+def attachment(path):
+    return convert_certificate_to_pdf("attachment", path)
 
 @app.route('/favicon.ico')
 def favicon():
--- nau/course/certificate/course_certificate_to_pdf.py
+++ nau/course/certificate/course_certificate_to_pdf.py
@@ -1,5 +1,8 @@
+import requests
 import pdfkit
 import logging as log
+from bs4 import BeautifulSoup
+from builtins import dict
 
 '''
 Class that converts and URL to a certificate to a PDF
@@ -14,13 +17,52 @@ class CourseCertificateToPDF:
         '''
         Receives and URL and returns the binary PDF.
         '''
-        http_header_name = self._config['HTTP_HEADER_NAME']
-        http_header_value = str(self._config['HTTP_HEADER_VALUE'])
 
-        pdf = pdfkit.from_url(url, False, options={
+        extracted_pdfkit_http_metas = self.extract_options_on_http_metas(url);
+        options_extracted_on_http_metas = self.removePdfKitPrefix(extracted_pdfkit_http_metas)
+
+        options_force_show_certificate_content = {
             'custom-header' : [
-               (http_header_name, http_header_value)
+               (self.http_header_name(), self.http_header_value())
            ]
-        })
+        }
+
+        options = {**options_force_show_certificate_content, **options_extracted_on_http_metas}
+
+        print(options_extracted_on_http_metas)
+
+        pdf = pdfkit.from_url(url, False, options=options)
 
         return pdf
+
+    def http_header_name(self):
+        return self._config['HTTP_HEADER_NAME']
+
+    def http_header_meta_prefix(self):
+        return self._config['HTTP_HEADER_META_PREFIX']
+
+    def http_header_value(self):
+        return str(self._config['HTTP_HEADER_VALUE'])
+
+    def extract_options_on_http_metas(self, url):
+        '''
+        Extracts a dictionary with all HTTP meta headers on the url that it's prefixed with 'pdfkit_'
+        '''
+        request_headers = { self.http_header_name() : self.http_header_value() }
+        response = requests.get(url, headers=request_headers)
+        soup = BeautifulSoup(response.text)
+
+        metas = soup.find_all('meta')
+        pdfkit_metas = dict()
+
+        for meta in metas:
+            if 'name' in meta.attrs and meta.attrs['name'].startswith(self.http_header_meta_prefix()):
+                pdfkit_metas.update( { meta.attrs['name'] : meta.attrs['content']} )
+
+        return pdfkit_metas
+
+    def removePdfKitPrefix(self, options):
+        '''
+        Remove pdf_kit prefix from each options dict key.
+        '''
+        return {key.replace(self.http_header_meta_prefix(), ''): value for key, value in options.items()}
